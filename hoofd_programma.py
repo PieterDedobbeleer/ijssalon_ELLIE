48820b52393dedac95c84acf2ca8d7413499a494
Dedobbeleer Pieter <pieter.dedobbeleer@telenet.be>
2025-03-24 12:40:51 +0100

    Presentatie en Boekhouding toegevoegd

---
+++ hoofd_programma.py
@@ -0,0 +1,11 @@
+from helper import onderstreep
+
+uitvoer = onderstreep("AANBIEDING")
+uitvoer.append("Aardbeienijs, emmertje van 5l : 5 Euro ")
+uitvoer.append("Slagroom, spuitbus van 1l : 2 Euro ")
+
+print()
+
+for el in uitvoer:
+    print(el)
+    
---
+++ presentatie.py
@@ -0,0 +1,6 @@
+def presenteer(totaal={}):
+    for k,v in totaal.items():
+        print(k,":",v," Euro ")
+    print(30*"=")
+    #total = sum(totaal.values())
+    #print(total)
